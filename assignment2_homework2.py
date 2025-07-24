41ce433b3c045836b64b93c421698a2879f0125f
Akshbhardwaj <73707186+Akshbhardwaj@users.noreply.github.com>
2025-07-24 19:50:21 +0530

    Add files via upload

---
+++ assignment2_homework2.py
@@ -0,0 +1,16 @@
+i=1
+a=0
+for i in range(0,50):
+   i+=1
+   a=a+i
+print("The sum of numbers from 1 to 50 is: " , a)
+
+#---------------------------------------------------------------------------------------------------------
+
+#num=int(input("Enter the last number till you the sum of numbers: "))
+#i=1
+#a=0
+#for i in range(0,num):
+#   i+=1
+#   a=a+i
+#print("The sum of numbers from 1 to" , num , "is: ",a)
